c7d4294c7494753b9ca94b8b968f12bb600ab7ec
Deepraj15 <138217166+Deepraj15@users.noreply.github.com>
2024-01-23 17:31:58 +0530

    newAddition

--- src/Components/AnnouncementBar/PaymentGetway.js
+++ src/Components/AnnouncementBar/PaymentGetway.js
@@ -1,7 +1,46 @@
+import GooglePayButton from '@google-pay/button-react';
 import React from 'react'
-
+import '../AnnouncementBar/PaymentGetway.css'
 export default function PaymentGetway() {
   return <>
-    <h1>This is a Payment GetWay</h1>
+    <div className='paymentdiv'>
+      <GooglePayButton
+        environment="TEST"
+        paymentRequest={{
+          apiVersion: 2,
+          apiVersionMinor: 0,
+          allowedPaymentMethods: [
+            {
+              type: 'CARD',
+              parameters: {
+                allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
+                allowedCardNetworks: ['MASTERCARD', 'VISA'],
+              },
+              tokenizationSpecification: {
+                type: 'PAYMENT_GATEWAY',
+                parameters: {
+                  gateway: 'example',
+                  gatewayMerchantId: 'exampleGatewayMerchantId',
+                },
+              },
+            },
+          ],
+          merchantInfo: {
+            merchantId: '12345678901234567890',
+            merchantName: 'Demo Merchant',
+          },
+          transactionInfo: {
+            totalPriceStatus: 'FINAL',
+            totalPriceLabel: 'Total',
+            totalPrice: '100.00',
+            currencyCode: 'USD',
+            countryCode: 'US',
+          },
+        }}
+        onLoadPaymentData={paymentRequest => {
+          console.log('load payment data', paymentRequest);
+        }}
+      />
+    </div>
   </>
 }
